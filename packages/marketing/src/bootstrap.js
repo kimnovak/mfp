4b801381e51a7d2f1259ee63f2ae62ffacec76ba
Kim Novak <kimnovak187@gmail.com>
2022-02-19 18:13:19 +0100

    Update navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,16 +1,30 @@
 import ReactDOM from 'react-dom'
 import React from 'react';
+import {createMemoryHistory, createBrowserHistory} from 'history';
 
 import App from './App';
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, {onNavigate, defaultHistory}) => {
+  const history = defaultHistory || createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history}  />, el);
+
+  return {
+    onParentNavigate(location) {
+      if(history.pathname !== location.pathname) {
+        history.push(location.pathname);
+      }
+    }
+  }
 };
 
 if (process.env.NODE_ENV === "development") {
   const el = document.querySelector("#_marketing-dev-root");
   if (el) {
-    mount(el);
+    mount(el, {defaultHistory: createBrowserHistory()});
   }
 }
 
